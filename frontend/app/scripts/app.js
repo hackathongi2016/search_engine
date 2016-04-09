3a0b35be0017327032d084a1540daae541e2348e
carlos <carlosvelasco69@hotmail.com>
2016-04-09 19:19:02 +0200

    cvb .

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -34,7 +34,6 @@ angular
                     }
                 },
                 url     : '/travels?param1',
-
                 views : {
                     'search'  : {
                         templateUrl  : 'views/main.html',
