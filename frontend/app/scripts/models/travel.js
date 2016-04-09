d3146394dab025457e7b0b212417332e35436d38
Sergi Diudé <sdiude@gmail.com>
2016-04-09 19:13:04 +0200

    .

--- frontend/app/scripts/models/travel.js
+++ frontend/app/scripts/models/travel.js
@@ -9,18 +9,18 @@
  */
 angular.module('searchEngineApp')
   .factory('Travel', function (Restangular, $timeout, $q) {
-    
+
     function Travel(data){
-        
+
         // defaults
         this.setFields({});
-        
+
         if(data){
-            this.setFields(data);            
+            this.setFields(data);
         }
         Restangular.restangularizeElement(this, "travels");
     }
-    
+
     Travel.prototype = {
         attributes: [
             'tra_id',
@@ -33,10 +33,10 @@ angular.module('searchEngineApp')
             'tra_long',
             'tra_budget_max',
             'tra_budget_min',
-            'tra_planning_limit', 
-            'tra_persons_min', 
-            'tra_persons_max', 
-            'tra_description', 
+            'tra_planning_limit',
+            'tra_persons_min',
+            'tra_persons_max',
+            'tra_description',
             'tra_usr_id'],
         setFields: function(data){
             var self = this;
@@ -47,14 +47,14 @@ angular.module('searchEngineApp')
             });
         }
     }
-    
+
     Travel.getList = function(query){
         /*return Restangular.one("travels").one("search").all(query).getList().then(function(data){
             return data.map(function(travel){
                 return new Travel(travel);
             });
         });*/
-        
+
         var tmpData = [
             {
                 tra_id             : 1,
@@ -64,8 +64,8 @@ angular.module('searchEngineApp')
                 tra_budget_min     : 150.23,
                 tra_budget_max     : 200.50,
                 tra_date           : '2016-05-20',
-                tra_lat            : 2.234,
-                tra_long           : 1.123,
+                tra_lat            : 54.779951,
+                tra_long           : 9.334164,
                 tra_planning_limit : '2016-05-04',
                 tra_persons_min    : 2,
                 tra_persons_max    : 4,
@@ -79,9 +79,9 @@ angular.module('searchEngineApp')
                 tra_num_days       : 10,
                 tra_budget_min     : 150.23,
                 tra_budget_max     : 200.50,
-                tra_date           : '2016-05-25    ',
-                tra_lat            : 2.234,
-                tra_long           : 1.123,
+                tra_date           : '2016-05-25',
+                tra_lat            : 52.975556,
+                tra_long           : 7.596811,
                 tra_planning_limit : '2016-05-05',
                 tra_persons_min    : 2,
                 tra_persons_max    : 4,
@@ -96,8 +96,8 @@ angular.module('searchEngineApp')
                 tra_budget_min     : 150.23,
                 tra_budget_max     : 200.50,
                 tra_date           : '2016-05-18',
-                tra_lat            : 2.234,
-                tra_long           : 1.123,
+                tra_lat            : 47.209613,
+                tra_long           : 15.991539,
                 tra_planning_limit : '2016-05-03',
                 tra_persons_min    : 2,
                 tra_persons_max    : 4,
@@ -112,7 +112,7 @@ angular.module('searchEngineApp')
         $timeout(function () { deferred.resolve( tmpTravels ); }, Math.random() * 1000, false);
         return deferred.promise;
     }
-      
+
     return Travel;
-      
+
   });
