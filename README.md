922f931363b858d914a65e1396f8cb1e4866701c
Walter Juan <walterjuanp@gmail.com>
2016-04-09 17:59:54 +0200

    Sync

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# search_engine
-Cerca de viatges
+# Travel.me (search engine)
+
+- **frontend: ?
+- **solr**: Custom Solr configuration
+- **solr_sync**: Solr synchronization in Ruby (using 1.9.3)
+
+~~~
+$ bundle install
+$ bundle exec ruby sync.rb
+~~~
--- solr_sync/sync.rb
+++ solr_sync/sync.rb
@@ -2,6 +2,7 @@ require 'active_record'
 require 'mysql2'
 require 'rsolr'
 
+puts ">>>> Start <<<<"
 puts '== Configuring database connection...'
 ActiveRecord::Base.establish_connection(
   :adapter => "mysql2",
@@ -20,7 +21,6 @@ class Route < ActiveRecord::Base
 end
 
 puts "===== Travels ====="
-puts "==================="
 puts '== Configuring Solr connection...'
 solr = RSolr.connect :url => 'http://localhost:8983/solr/travels'
 
@@ -31,15 +31,24 @@ solr.commit
 puts '== Synchronizing...'
 Travel.find_each do |travel|
   solr.add :id => travel.id,
-    :tra_description => travel.tra_description,
+    :tra_origin => travel.tra_origin,
     :tra_destination => travel.tra_destination,
-    :tra_origin => travel.tra_origin
+    :tra_num_days => travel.tra_num_days,
+    :tra_budget_max => travel.tra_budget_max,
+    :tra_budget_min => travel.tra_budget_min,
+    :tra_date => travel.tra_date,
+    :tra_planning_limit => travel.tra_planning_limit,
+    :tra_persons_min => travel.tra_persons_min,
+    :tra_persons_max => travel.tra_persons_max,
+    :tra_description => travel.tra_description,
+    :tra_usr_id => travel.tra_usr_id,
+    :tra_lat => travel.tra_lat,
+    :tra_long => travel.tra_lat
   print '.'
 end
 solr.commit
 puts ""
 
-
 puts "===== Routes ====="
-puts "=================="
 puts "WIP..."
+puts ">>>> Finish <<<<"
