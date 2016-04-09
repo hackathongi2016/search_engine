f908dba16a76c283e939be622b4a5a5a2415efe9
carlos <carlosvelasco69@hotmail.com>
2016-04-09 10:36:51 +0200

    commit frontend + yeoman

---
+++ frontend/app/scripts/controllers/mynewcontroller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name searchEngineApp.controller:MynewcontrollerCtrl
+ * @description
+ * # MynewcontrollerCtrl
+ * Controller of the searchEngineApp
+ */
+angular.module('searchEngineApp')
+  .controller('MynewcontrollerCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
